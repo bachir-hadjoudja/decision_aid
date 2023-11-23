1c63fb4c42162298728c2ed43febc5c78f0bffa6
Hadjoudja Bachir <bachir.hadjoudja@etu.umontpellier.fr>
2023-11-23 18:11:00 +0100

    mesures modif

--- mariagesStable_final.py
+++ mariagesStable_final.py
@@ -3,6 +3,8 @@ import copy
 from turtle import distance
 import sys
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import numpy as np
 
 #function to read and return data from a json file
 def retrieve_data(filename):
@@ -17,9 +19,9 @@ def gale_shapley(proposers, students):
 
     while student_queue:
         student = student_queue.pop(0)
-        print("student", student)
+        #print("student", student)
         student_preferences = students[student]
-        print("student_prefe", student_preferences)
+        #print("student_prefe", student_preferences)
 
         if student_preferences:
             proposer = student_preferences.pop(0)
@@ -58,9 +60,9 @@ def is_stable_match(proposer_preferences, student_preferences, matching):
     nb_blocking_matches = 0
     # Iterate through each proposeur
     for proposer, preferences in proposer_preferences.items():
-        print("#####testing for : "+ proposer)
+        #print("#####testing for : "+ proposer)
         for student in preferences:
-            print("##testing for : "+ student)
+            #print("##testing for : "+ student)
             #condition d'arret: aucun des students itérés percedemment ne prefere l'offre courrante
             if student == current_engagements.get(proposer):
                 break  # The current engagement is already stable
@@ -81,143 +83,55 @@ def is_stable_match(proposer_preferences, student_preferences, matching):
         return False
 
 
-# import matplotlib.pyplot as plt
-
-# def analyse_moyenne(stable_matches, proposers_preferences, students_preferences):
-#     proposer_match = stable_matches[0]
-#     student_match = stable_matches[1]
-#     nb_participants = len(proposer_match) + len(student_match)
-#     print("proposer_matches == ", proposer_match)
-#     distance_count = 0
-#     distance_student = 0
-#     distance_etablissement = 0
-#     moyennes = []
-#     moyennes_students = []  # Liste pour stocker les moyennes à chaque itération
-#     moyennes_etablissements=[]
-
-
-#     students = list(student_match.keys())  # Obtenez une liste des étudiants
-#     proposals = list(student_match.values())
-#     for p, s in proposer_match.items():
-#         # get proposer priorities
-#         p_preferences = proposers_preferences[p]
-#         # get student priorities
-#         s_preferences = students_preferences[s]
-#         # get proposer index in student priorities
-#         assigned_student = p_preferences.index(s)
-#         # get student index in proposer priorities
-#         assigned_proposal = s_preferences.index(p)
-
-#         distance_student = distance_student + assigned_student
-#         distance_etablissement = distance_etablissement + assigned_proposal
-#         # add found indices
-#         distance_count = distance_count + assigned_proposal + assigned_student
-        
-#         moyenne_student = (distance_student / len(student_match))*100
-#         moyenne_etablissement = (distance_etablissement / len(proposer_match))*100
-
-#         # calculate moyenne and store it
-#         moyenne = (distance_count / nb_participants)*100
-#         moyennes.append(moyenne)
-#         moyennes_students.append(moyenne_student)
-#         moyennes_etablissements.append(moyenne_etablissement)
-#     print("distance totale == " + str(distance_count))
-#     print("moyenne == " + str(moyenne))
-
-#     # Plotting the moyennes
-#     plt.plot(students, moyennes, marker='o', linestyle='-')
-#     #plt.plot(range(1, len(moyennes)+1), moyennes)
-#     #plt.plot(students, moyennes_students, marker='o', linestyle='-')
-#     #plt.plot(range(1, len(moyennes_etablissements)+1), moyennes_etablissements)
-#     #plt.plot(proposals, moyennes_etablissements, marker='o', linestyle='-')
-#     #plt.plot(range(1, len(moyennes_students)+1), moyennes_students)
-#     plt.xlabel('Les etudiants/etablissements')
-#     plt.ylabel('Moyenne des distances')
-#     plt.title('Évolution de la moyenne des distances')
-#     plt.show()
-
-#     return moyenne
-
-
+#La moyenne des positions des étudiants et des établissements
 def analyse_moyenne(stable_matches, proposers_preferences, students_preferences):
     proposer_match = stable_matches[0]
+    print("proposer_match", proposer_match)
+    print("len(proposer_match)", len(proposer_match))
     student_match = stable_matches[1]
     nb_participants = len(proposer_match) + len(student_match)
-    print("proposer_matches == ", proposer_match)
-    distance_count = 0
+
     distance_student = 0
     distance_etablissement = 0
-    moyennes = []
     moyennes_students = []  # Liste pour stocker les moyennes à chaque itération
     moyennes_etablissements = []
 
     students = list(student_match.keys())  # Obtenez une liste des étudiants
-    proposals = list(student_match.values())
+    proposals = list(proposer_match.values())  # Liste des établissements
 
     for p, s in proposer_match.items():
-        # get proposer priorities
+        # get proposer and student priorities
         p_preferences = proposers_preferences[p]
-        # get student priorities
         s_preferences = students_preferences[s]
-        # get proposer index in student priorities
-        assigned_student = p_preferences.index(s)
-        # get student index in proposer priorities
-        assigned_proposal = s_preferences.index(p)
 
+        # calculate distances and store them
+        assigned_student = p_preferences.index(s)
+        assigned_proposal = s_preferences.index(p)
         distance_student += assigned_student
         distance_etablissement += assigned_proposal
-        # add found indices
-        distance_count += assigned_proposal + assigned_student
-        
-        # calculate moyenne and store it
-        moyenne_student = (distance_student / len(proposer_match)) * 100
-        moyenne_etablissement = (distance_etablissement / len(student_match)) * 100
-        moyenne = (distance_count / nb_participants) * 100
 
+    moyenne_students = distance_student / len(proposer_match) * 100
+    moyenne_proposers = distance_etablissement / len(student_match) * 100
 
-        moyennes.append(moyenne)
-        moyennes_students.append(moyenne_student)
-        moyennes_etablissements.append(moyenne_etablissement)
+    return moyenne_proposers, moyenne_students
 
-    print("distance totale == " + str(distance_count))
-    print("moyenne == " + str(moyenne))
-
-    # Plotting the moyennes
-    plt.plot(students, moyennes_students, label='Moyenne par étudiant', marker='o', linestyle='-')
-    plt.plot(proposals, moyennes_etablissements, label='Moyenne par établissement', marker='o', linestyle='-')
-    plt.plot(students, moyennes, label='Moyenne totale', marker='o', linestyle='-')
-
-    plt.xlabel('Les étudiants/établissements')
-    plt.ylabel('Moyenne des distances (%)')
-    plt.title('Évolution de la moyenne des distances')
-    plt.legend()
-    plt.show()
-
-    return moyenne
-
-
-# Exemple d'utilisation
-# Supposons que vous avez des données appropriées pour stable_matches, proposers_preferences, et students_preferences
-# analyse_moyenne(stable_matches, proposers_preferences, students_preferences)
-
-#Positionnement par rapport au pire des cas
-def analyse_ratio_satisfaction(stable_matches, proposers_preferences, students_preferences, moyenne):
+#Positionnement par rapport au pire des cas -- une autre approche
+def analyse_ratio_satisfaction(stable_matches, proposers_preferences, students_preferences):
     proposer_match = stable_matches[0]
     student_match = stable_matches[1]
-    print("proposer_matches == ",proposer_match)
-    nb_participants = len(proposer_match)+len(student_match)
-    distance_totale=0
-    for p,s in proposer_match.items(): 
-        #get proposer priorities
-        p_preferences = proposers_preferences[p]
-        distance_totale += len(p_preferences)
-        #get student priorities
-        s_preferences = students_preferences[s]
-        distance_totale += len(s_preferences)
-    #print("pire distance possible == " + str(distance_totale))
-    ratio = (distance_totale - (moyenne*nb_participants)) / distance_totale # TODO : ?????
-    print("ratio == "+ str(ratio))
-    return ratio
+
+    # Calcul de la distance totale actuelle et du pire des cas pour les proposers
+    proposer_distance_actuelle = sum(proposers_preferences[p].index(proposer_match[p]) for p in proposer_match)
+    proposer_pire_distance = sum(len(proposers_preferences[p]) for p in proposer_match)
+
+    # Calcul de la distance totale actuelle et du pire des cas pour les students
+    student_distance_actuelle = sum(students_preferences[s].index(student_match[s]) for s in student_match)
+    student_pire_distance = sum(len(students_preferences[s]) for s in student_match)
+    # Calcul des ratios
+    ratio_proposers = 1 - (proposer_distance_actuelle / proposer_pire_distance)
+    ratio_students = 1 - (student_distance_actuelle / student_pire_distance)
+
+    return ratio_proposers, ratio_students
 
 #Ceux qui ont eu leur premier choix 
 def analyse_utilitariste(stable_matches, proposers_preferences, students_preferences):
@@ -238,80 +152,86 @@ def analyse_utilitariste(stable_matches, proposers_preferences, students_prefere
     ratio_students = student_first_choice / len(students_preferences)
     ratio_etablissements = proposer_first_choice / len(proposers_preferences)
     print("ratio premier_choix students / total students == " + str(ratio_students))
-    print("ratio premier_choix etablissements / total etablissements == " + str(ratio_etablissementss))
+    print("ratio premier_choix etablissements / total etablissements == " + str(ratio_etablissements))
 
 
-#TODO: define
-
-def analyse_egalitariste(stable_matches, proposers_preferences, students_preferences):
+def analyse_pour_chaque_choix(stable_matches, proposers_preferences, students_preferences):
     proposer_match = stable_matches[0]
     student_match = stable_matches[1]
-    proposer_first_choice = 0
-    student_first_choice = 0
-    for p,s in proposer_match.items():
-        #get proposer prprecision_rangiorities
+    max_preference = max(len(proposers_preferences[p]) for p in proposers_preferences)
+    
+    score_proposers = {i: 0 for i in range(1, max_preference + 1)}
+    score_students = {i: 0 for i in range(1, max_preference + 1)}
+
+    for p, s in proposer_match.items():
         p_preferences = proposers_preferences[p]
-        #get student priorities
         s_preferences = students_preferences[s]
-        if proposer_match[p] == p_preferences[0]:
-            proposer_first_choice=proposer_first_choice+1
-        if student_match[s] == s_preferences[0]:
-            student_first_choice = student_first_choice +1
-    total = len(proposers_preferences) + len(students_preferences)
-    ratio = (student_first_choice + proposer_first_choice) / total
-    print("ratio premier_choix / total == " + str(ratio))
+
+        p_choice_index = p_preferences.index(proposer_match[p]) + 1
+        s_choice_index = s_preferences.index(student_match[s]) + 1
+
+        score_proposers[p_choice_index] += 1
+        score_students[s_choice_index] += 1
+
+    return score_proposers, score_students
 
 
+
+def analyse_egalitariste_variance(stable_matches, proposers_preferences, students_preferences):
+    distances_proposers = []
+    distances_students = []
+
+    proposer_match = stable_matches[0]
+    student_match = stable_matches[1]
+
+    # Calcul des distances pour proposers et students
+    for p, s in proposer_match.items():
+        p_preferences = proposers_preferences[p]
+        s_preferences = students_preferences[s]
+
+        proposer_distance = p_preferences.index(s) + 1
+        student_distance = s_preferences.index(p) + 1
+
+        distances_proposers.append(proposer_distance)
+        distances_students.append(student_distance)
+
+    # Calcul des moyennes pour chaque groupe
+    moyenne_proposers = sum(distances_proposers) / len(distances_proposers)
+    moyenne_students = sum(distances_students) / len(distances_students)
+
+    # Calcul des variances pour chaque groupe
+    variance_proposers = sum(pow((distance - moyenne_proposers), 2) for distance in distances_proposers) / len(distances_proposers)
+    variance_students = sum(pow((distance - moyenne_students), 2) for distance in distances_students) / len(distances_students)
+
+    return variance_proposers, variance_students
+
+def gaussienne(x, moyenne, variance):
+    return (1 / np.sqrt(2 * np.pi * variance)) * np.exp(- (x - moyenne) ** 2 / (2 * variance))
+
+
+def calculer_poids(rang, nombre_total):
+    # Exemple : poids décroissant linéairement
+    return 1 - (rang - 1) / nombre_total
 
 def analyse_ponderee(stable_matches, proposers_preferences, students_preferences):
+    score_total = 0
+
     proposer_match = stable_matches[0]
     student_match = stable_matches[1]
-    nb_participants = len(proposer_match)+len(student_match) 
-    total_scores_ponderes = 0
-    for p,s in proposer_match.items(): 
-        #get proposer priorities
+
+    for p, s in proposer_match.items():
         p_preferences = proposers_preferences[p]
-        #get student priorities
         s_preferences = students_preferences[s]
-        #get proposer index in student priorities
-        assigned_student = p_preferences.index(s)
-        #gte student index in proposer  priorities
-        assigned_proposal = s_preferences.index(p)
-        p_score_pondere = 1 - (assigned_student/len(p_preferences))
-        s_score_pondere = 1 - (assigned_proposal/len(s_preferences))
-        #add found indeces
-        total_scores_ponderes = total_scores_ponderes + s_score_pondere + p_score_pondere
-    #print("pire distance possible == " + str(distance_totale))
-    print("distance totale == " + str(total_scores_ponderes))
-    moyenne =  total_scores_ponderes / nb_participants #moyenne des distances
-    print("moyenne scores ponderes == "+ str(moyenne))
-    return moyenne
 
+        rang_proposer = p_preferences.index(s)
+        rang_student = s_preferences.index(p)
 
+        poids_proposer = calculer_poids(rang_proposer + 1, len(p_preferences))
+        poids_student = calculer_poids(rang_student + 1, len(s_preferences))
 
-def analyse_variance(stable_matches, proposers_preferences, students_preferences, moyenne):
-    #retrieve priorityh of assigned match
-    sigma = 0
-    proposer_match = stable_matches[0]
-    student_match = stable_matches[1]
-    for p,s in proposer_match.items(): 
-        #get proposer prprecision_rangiorities
-        p_preferences = proposers_preferences[p]
-        #get student priorities
-        s_preferences = students_preferences[s]
-        #get proposer index in student priorities
-        assigned_student_priority = p_preferences.index(s)
-        #gte student index in proposer  priorities
-        assigned_proposal_priority = s_preferences.index(p)
-        sigma = sigma + pow((assigned_student_priority - moyenne),2)
-        sigma = sigma + pow((assigned_proposal_priority - moyenne),2)
-        variance = sigma/8
-    print("variance = ", variance)
+        score_total += poids_proposer + poids_student
 
-
-
-
-
+    return score_total
 
 
 
@@ -333,21 +253,84 @@ if __name__ == "__main__":
     proposersX=copy.deepcopy(proposers)
     studentsX = copy.deepcopy(students)
     print("\n "+ str(proposers))
+    print("\n "+str(students))
 
     stable_matches = gale_shapley(proposers, students)
+
+
+
 
     isStable = is_stable_match(proposersX, studentsX, stable_matches)
     print("\n::stable_matches :: ")
     print(stable_matches)
 
-    moyenne = analyse_moyenne(stable_matches, proposersX, studentsX)
-    analyse_ratio_satisfaction(stable_matches, proposersX, studentsX, moyenne)
+
+    # Utilisez cette fonction avec vos données
+    score_proposers, score_students = analyse_pour_chaque_choix(stable_matches, proposersX, studentsX)
+
+    # Générer une palette de couleurs
+    colors_proposers = list(mcolors.TABLEAU_COLORS.values())
+    colors_students = list(mcolors.CSS4_COLORS.values())
+
+    # Assurez-vous d'avoir assez de couleurs
+    if len(colors_proposers) < len(score_proposers):
+        colors_proposers += list(mcolors.CSS4_COLORS.values())[:len(score_proposers) - len(colors_proposers)]
+    if len(colors_students) < len(score_students):
+        colors_students += list(mcolors.TABLEAU_COLORS.values())[:len(score_students) - len(colors_students)]
+
+    # Création des histogrammes
+    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+
+    axes[0].bar(score_proposers.keys(), score_proposers.values(), color=colors_proposers)
+    axes[0].set_title('Établissements')
+    axes[0].set_xlabel('Choix')
+    axes[0].set_ylabel('Nombre de Correspondances')
+
+    axes[1].bar(score_students.keys(), score_students.values(), color=colors_students)
+    axes[1].set_title('Étudiants')
+    axes[1].set_xlabel('Choix')
+
+    plt.tight_layout()
+    plt.show()
+
+
+    # Exemple d'utilisation
+    ratio_proposers, ratio_students = analyse_ratio_satisfaction(stable_matches, proposersX, studentsX)
+    print("Ratio de satisfaction pour les proposers :", ratio_proposers)
+    print("Ratio de satisfaction pour les étudiants :", ratio_students)
+
+
+    moyenne_students, moyenne_proposers = analyse_moyenne(stable_matches, proposersX, studentsX)
+    #analyse_ratio_satisfaction(stable_matches, proposersX, studentsX, moyenne)
     analyse_utilitariste(stable_matches, proposersX, studentsX)
-    analyse_variance(stable_matches, proposersX, studentsX, moyenne)
-    analyse_ponderee(stable_matches, proposersX, studentsX)
+    
+    
+    # Exemple d'utilisation
+    variance_proposers, variance_students = analyse_egalitariste_variance(stable_matches, proposersX, studentsX)
+    print("Variance pour les proposers :", variance_proposers)
+    print("Variance pour les étudiants :", variance_students)
 
-    # gini_proposers = calculate_gini_coefficient(proposersX)
-    # gini_students = calculate_gini_coefficient(studentsX)
 
-    # print(f"Gini coefficient for proposers: {gini_proposers}")
-    # print(f"Gini coefficient for students: {gini_students}")
+
+    # Calcul des variances et des moyennes (Utilisez votre fonction ici)
+    variance_proposers, variance_students = analyse_egalitariste_variance(stable_matches, proposersX, studentsX)
+
+    # Plage de x autour des moyennes
+    x = np.linspace(0, max(moyenne_proposers, moyenne_students) + 3, 100)
+
+    # Calcul des y pour les gaussiennes
+    y_proposers = gaussienne(x, moyenne_proposers, variance_proposers)
+    y_students = gaussienne(x, moyenne_students, variance_students)
+
+    # Tracer les courbes
+    plt.plot(x, y_proposers, label='Proposers')
+    plt.plot(x, y_students, label='Students')
+    plt.title('Distribution de Satisfaction (Gaussienne)')
+    plt.xlabel('Rang des Choix')
+    plt.ylabel('Densité de Probabilité')
+    plt.legend()
+    plt.show()
+
+    # Exemple d'utilisation
+    score_pondere = analyse_ponderee(stable_matches, proposersX, studentsX)
+    print("Score pondéré total :", score_pondere)
